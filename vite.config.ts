57e03237498d53d581f1e64ba01d19c9d35fae52
iujunhyung <junehyung@iyulab.com>
2025-11-13 19:29:49 +0900

    update

--- vite.config.ts
+++ vite.config.ts
@@ -12,10 +12,12 @@ export default defineConfig({
     target: 'esnext',
     outDir: 'dist',
     emptyOutDir: true,
-    sourcemap: false,
+    copyPublicDir: false,
+    minify: false,
     lib: {
-      name: 'DataComponents',
-      entry: resolve(__dirname, 'src/index.ts'),
+      entry: [
+        resolve(__dirname, 'src/index.ts'),
+      ],
       formats: ['es'],
       fileName: (format, entry) => {
         return format === 'es' ? `${entry}.js` : `${entry}.${format}.js`;
@@ -25,16 +27,12 @@ export default defineConfig({
       external: [
         /^@iyulab.*/,
         /^lit.*/,
-        /^@lit.*/,
         /^react.*/,
         /^devextreme.*/,
-        'reflect-metadata',
       ],
       output: {
-        preserveModulesRoot: 'src',
         preserveModules: true,
-        assetFileNames: 'assets/[name]-[hash].[extname]',
-        chunkFileNames: 'chunks/[name]-[hash].js',
+        preserveModulesRoot: 'src',
       }
     }
   },
