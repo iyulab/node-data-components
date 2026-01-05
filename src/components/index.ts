1f1c86974e7e759914b452ce58c3e8c0324056c5
uj <caveman@iyulab.com>
2026-01-05 20:07:44 +0900

    fix: update data-grid exports after index.ts deletion

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
-export * from './data-grid';
+export { UDataGrid } from './data-grid/UDataGrid';
+export type { UDataGridColumn, UDataGridProps } from './data-grid/UDataGrid.types';
 export * from './data-view/UDataView';
