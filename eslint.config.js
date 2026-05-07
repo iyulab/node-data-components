d165f66232886256b39a44f9ba46b3f60a0cf3ab
uj <caveman@iyulab.com>
2026-05-07 09:02:32 +0900

    chore: remove React eslint plugins from config

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from 'eslint/config';
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
 import globals from 'globals';
 
 export default defineConfig([
-  { 
-    ignores: ['dist/**', 'node_modules/**'] 
+  {
+    ignores: ['dist/**', 'node_modules/**']
   },
   {
     ...eslint.configs.recommended,
@@ -20,18 +18,9 @@ export default defineConfig([
         tsconfigRootDir: import.meta.dirname
       }
     },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': ['warn', { 
+      '@typescript-eslint/no-unused-vars': ['warn', {
         'argsIgnorePattern': '^_',
         'varsIgnorePattern': '^_'
       }],
